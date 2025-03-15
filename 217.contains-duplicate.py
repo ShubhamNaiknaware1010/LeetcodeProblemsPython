2661018ee347eef242c68998a269db78d661c5ee
Shubham Naiknaware <shubhamnaiknaware1010@gmail.com>
2025-03-16 01:28:45 +0530

    Two Files Edited

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -3,15 +3,26 @@
 #
 # [217] Contains Duplicate
 #
+"""
+
+
+
+"""
 
 # @lc code=start
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
+        # Creates  a set to store nums values in set
+        s = set()   
+        # Iterate over the loop 
         for num in nums:
+            # check if number exists in set 
             if num in s:
+                # return true if number exists
                 return True
+            # if number does not exist add it to set
             s.add(num)
         # return len(nums) !=  len(set(nums))
+        #return false if no duplicates found
         return False
 
---
+++ 242.valid-anagram.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """if len(s) != len(t):
+            return False
+        d = dict()
+        for letter in s:
+            d[letter] = d.get(letter,0) + 1
+        for letter in t:
+            d[letter] = d.get(letter,0) - 1
+            if d[letter] < 0:
+                return False 
+        return True
+                """
+        return Counter(s) == Counter(t) 
+        
+
+                
+# @lc code=end
